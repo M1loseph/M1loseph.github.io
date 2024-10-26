9cc61ab5e6e6837604a6b6dda0204d3424b40707
M1loseph <milosz.dyka@gmail.com>
2024-10-26 19:32:03 +0200

    deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "623195049"
+    serviceWorkerVersion: "3581070431"
   }
 });
